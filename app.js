f246b87aee3d307ff912bc3e654e74ff90a5779e
Alaneme Ikenna Raphael <ialaneme@yahoo.com>
2020-08-27 22:38:17 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,26 +1,61 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const app = express();
 
+
+
 // HANDLING MY MIDDLEWARES
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-
-app.post('/groceries', (req, res, next) => {
-    const product = {
-        name: req.body.name,
-        description: req.body.description
+// SETTING CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === 'OPTIONS') {
+        res.header("Access-Control-Allow-Methods", "PUT, PATCH, GET, POST, DELETE")
+        return res.status(200).json({});
     }
-    res.status(201).json({
-        message: 'Product created'
-    })
+    next();
+})
 
-    console.log(product);
+
+app.get('/products', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling request to get products'
+    })
+})
+
+app.post('/products', (req, res, next) => {
+    console.log(req.body)
+    res.status(200).json({
+        message: 'Handling request to post products'
+    })
+})
+
+
+
+// HANDLING ERRORS FOR MISPLACED ROUTES
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
 });
 
+
+// HANDLING ANY OTHER ERRORS
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 
 const port = process.env.PORT || 3001;
@@ -29,3 +64,4 @@ const port = process.env.PORT || 3001;
 app.listen(port, (req, res) => {
     console.log(`Server on port ${port}`);
 });
+
